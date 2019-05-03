6880817d44058658245f63efb06a40718b6f1441
jeffrey-pascal <jeffreypascal75@gmail.com>
2019-05-03 08:47:03 -0400

    Update SumobotCode_V1.ino

--- SumobotCode_V1/SumobotCode_V1.ino
+++ SumobotCode_V1/SumobotCode_V1.ino
@@ -1,16 +1,11 @@
 #include "Adafruit_VL53L0X.h" //distance sensor
 Adafruit_VL53L0X lox = Adafruit_VL53L0X(); // "
+#include <Servo.h>
+Servo liftservo;
 
 
 
-
-
-
-#define brake_servo   5
-#define linesens_back 7
-#define linesens_left 8
-#define linesens_right 2
-#define lift_servo 3
+#define lift_servo   5
 #define motor_left_1 6
 #define motor_left_2 9
 #define motor_right_1 10
@@ -19,10 +14,14 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X(); // "
 int distance;
 
 void setup() {
+  liftservo.attach(lift_servo);
+  pinMode(motor_left_1, OUTPUT);
+  pinMode(motor_left_2, OUTPUT);
+  pinMode(motor_right_1, OUTPUT);
+  pinMode(motor_right_2, OUTPUT);
   Serial.begin(9600);
-  delay(5000); // startup delay
-  analogWrite(brake_servo, 0); //setting servo to proper position
-  analogWrite(lift_servo, 0);  //             "
+  //delay(5000); // startup delay
+  //liftservo.write(0); //setting servo to proper position
   lox.begin();
 }
 
@@ -30,10 +29,24 @@ void loop() {
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
   distance = measure.RangeMilliMeter;
-  if (distance == 0) {
+  Serial.println(distance);
+  if (distance > 50) {
     analogWrite(motor_left_1, 255);
     digitalWrite(motor_left_2, LOW);
     analogWrite(motor_right_1, 255);
     digitalWrite(motor_right_2, LOW);
   }
+  else {
+    digitalWrite(motor_left_1, LOW);
+    digitalWrite(motor_left_2, LOW);
+    digitalWrite(motor_right_1, LOW);
+    digitalWrite(motor_right_2, LOW);
+  }
+/*  if (distance <= 50) {
+    liftservo.write(170);
+  }
+  else {
+    liftservo.write(0);
+  }
+  */
 }
