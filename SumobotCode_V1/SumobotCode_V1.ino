600de6e6ac76bfd93e11b7f77ba68ae3a0e20a1a
jeffrey-pascal <jeffreypascal75@gmail.com>
2019-05-06 09:48:10 -0400

    Update SumobotCode_V1.ino

--- SumobotCode_V1/SumobotCode_V1.ino
+++ SumobotCode_V1/SumobotCode_V1.ino
@@ -1,9 +1,13 @@
 #include "Adafruit_VL53L0X.h" //distance sensor
 Adafruit_VL53L0X lox = Adafruit_VL53L0X(); // "
 #include <Servo.h>
+#include <QTRSensors.h>
+
 Servo liftservo;
+QTRSensors qtr;
 
-
+const uint8_t SensorCount = 3;
+uint16_t sensorValues[SensorCount];
 
 #define LIFTSERVO   5
 #define MOTORLEFT_1 6
@@ -15,19 +19,31 @@ Servo liftservo;
 #define LINEFOUNDRIGHT 1
 #define ROBOTFOUND 2
 #define SEARCH 3
+#define FORWARD 4
+#define REVERSE 5
+#define LEFT 6
+#define RIGHT 7
 //analogread 5 and 4 are the distance sensor
 int distance;
 int state = SEARCH;
-int leftsens = analogRead(1);
-int rightsens = analogRead(2);
-int backsens = analogRead(3);
+int leftsens = analogRead(0);
+int rightsens = analogRead(1);
+int backsens = analogRead(2);
+
+long timeThis, timeLast;
+
 
 void forward() {
   analogWrite(MOTORLEFT_1, 255);
   digitalWrite(MOTORLEFT_2, LOW);
   analogWrite(MOTORRIGHT_1, 255);
   digitalWrite(MOTORRIGHT_2, LOW);
-  delay(500);
+  Serial.print("forward");
+  delay(100);
+  digitalWrite(MOTORLEFT_1, LOW);
+  digitalWrite(MOTORLEFT_2, LOW);
+  digitalWrite(MOTORRIGHT_1, LOW);
+  digitalWrite(MOTORRIGHT_2, LOW);
 }
 
 void reverse() {
@@ -35,7 +51,11 @@ void reverse() {
   analogWrite(MOTORLEFT_2, 255);
   digitalWrite(MOTORRIGHT_1, LOW);
   analogWrite(MOTORRIGHT_2, 255);
-  delay(500);
+  delay(100);
+  digitalWrite(MOTORLEFT_1, LOW);
+  digitalWrite(MOTORLEFT_2, LOW);
+  digitalWrite(MOTORRIGHT_1, LOW);
+  digitalWrite(MOTORRIGHT_2, LOW);
 }
 
 void left() {
@@ -43,7 +63,11 @@ void left() {
   digitalWrite(MOTORLEFT_2, LOW);
   digitalWrite(MOTORRIGHT_1, LOW);
   analogWrite(MOTORRIGHT_2, 255);
-  delay(500);
+  delay(100);
+  digitalWrite(MOTORLEFT_1, LOW);
+  digitalWrite(MOTORLEFT_2, LOW);
+  digitalWrite(MOTORRIGHT_1, LOW);
+  digitalWrite(MOTORRIGHT_2, LOW);
 }
 
 void right() {
@@ -51,33 +75,61 @@ void right() {
   analogWrite(MOTORLEFT_2, 255);
   analogWrite(MOTORRIGHT_1, 255);
   digitalWrite(MOTORRIGHT_2, LOW);
-  delay(500);
+  delay(100);
+  digitalWrite(MOTORLEFT_1, LOW);
+  digitalWrite(MOTORLEFT_2, LOW);
+  digitalWrite(MOTORRIGHT_1, LOW);
+  digitalWrite(MOTORRIGHT_2, LOW);
 }
 
 void setup() {
   liftservo.attach(LIFTSERVO);
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]) {
+    A1, A2, A3
+  }, SensorCount);
   pinMode(MOTORLEFT_1, OUTPUT);
   pinMode(MOTORLEFT_2, OUTPUT);
   pinMode(MOTORRIGHT_1, OUTPUT);
   pinMode(MOTORRIGHT_2, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (uint16_t i = 0; i < 400; i++) {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
   //delay(5000); // startup delay
   //liftservo.write(0); //setting servo to proper position
   lox.begin();
 }
 
 void loop() {
+  timeThis = millis();
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
   distance = measure.RangeMilliMeter;
   Serial.print(distance);
-  Serial.print("/t");6+98
+  Serial.print("\t");
 
-  
+
   Serial.println(state);
   switch (state) {
     case LINEFOUNDLEFT:
-      reverse;
+      state = REVERSE;
       right;
       state = SEARCH;
       break;
@@ -87,8 +139,13 @@ void loop() {
       state = SEARCH;
       break;
     case ROBOTFOUND:
-      forward;
-      if 
+      forward();
+      if (distance > 1000) {
+        state = SEARCH;
+      }
+      if (distance <= 100) {
+        //liftservo.write(170);
+      }
       break;
     case SEARCH:
       if (leftsens > 500) {
@@ -97,10 +154,18 @@ void loop() {
       if (rightsens > 500) {
         state = LINEFOUNDRIGHT;
       }
-      if (distance < 1000) {
+      if (distance <= 1000) {
         state = ROBOTFOUND;
       }
+      else {
+        right;
+        forward;
+        left;
+        forward;
+      }
+
   }
+
 
   /*  if (distance <= 50) {
       liftservo.write(170);
