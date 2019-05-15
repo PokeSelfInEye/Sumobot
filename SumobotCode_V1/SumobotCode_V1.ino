f602ed8cb8caf5b743dace927d31d7f75c880297
jeffrey-pascal <jeffreypascal75@gmail.com>
2019-05-15 09:17:23 -0400

    Update SumobotCode_V1.ino

--- SumobotCode_V1/SumobotCode_V1.ino
+++ SumobotCode_V1/SumobotCode_V1.ino
@@ -1,13 +1,11 @@
 #include "Adafruit_VL53L0X.h" //distance sensor
 Adafruit_VL53L0X lox = Adafruit_VL53L0X(); // "
-#include <Servo.h>
-Servo liftservo;
 
 #define LIFTSERVO   5
 #define MOTORLEFT_1 6
 #define MOTORLEFT_2 9
 #define MOTORRIGHT_1 10
-#define MOTORRIGHT_2 11
+#define MOTORRIGHT_2 5
 #define LEFTSENS 7
 #define RIGHTSENS 4
 #define BACKSENS 8
@@ -17,97 +15,31 @@ Servo liftservo;
 #define SEARCH 3
 #define LINEFOUNDBACK 4
 #define FORWARD 5
-#define BACKWARD 6
+#define REVERSE 6
 #define LEFT 7
 #define RIGHT 8
 //analogread 5 and 4 are the distance sensor
 int distance;
 int state = SEARCH;
+int direct;
 
 long timeThis, timeLast;
 
-
-void forward() {
-  if (timeThis - timeLast <= 100) {
-    analogWrite(MOTORLEFT_1, 255);
-    digitalWrite(MOTORLEFT_2, LOW);
-    analogWrite(MOTORRIGHT_1, 255);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-  else {
-    digitalWrite(MOTORLEFT_1, LOW);
-    digitalWrite(MOTORLEFT_2, LOW);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-}
-
-void reverse() {
-  if (timeThis - timeLast <= 100) {
-    digitalWrite(MOTORLEFT_1, LOW);
-    analogWrite(MOTORLEFT_2, 200);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    analogWrite(MOTORRIGHT_2, 200);
-  }
-  else {
-    digitalWrite(MOTORLEFT_1, LOW);
-    digitalWrite(MOTORLEFT_2, LOW);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-}
-
-void left() {
-  if (timeThis - timeLast <= 250) {
-    analogWrite(MOTORLEFT_1, 200);
-    digitalWrite(MOTORLEFT_2, LOW);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    analogWrite(MOTORRIGHT_2, 200);
-  }
-  else {
-    digitalWrite(MOTORLEFT_1, LOW);
-    digitalWrite(MOTORLEFT_2, LOW);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-}
-
-void right() {
-  if (timeThis - timeLast <= 250) {
-    digitalWrite(MOTORLEFT_1, LOW);
-    analogWrite(MOTORLEFT_2, 255);
-    analogWrite(MOTORRIGHT_1, 255);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-  else {
-    digitalWrite(MOTORLEFT_1, LOW);
-    digitalWrite(MOTORLEFT_2, LOW);
-    digitalWrite(MOTORRIGHT_1, LOW);
-    digitalWrite(MOTORRIGHT_2, LOW);
-  }
-}
-
 void setup() {
   Serial.begin(9600);
-  liftservo.attach(LIFTSERVO);
   pinMode(MOTORLEFT_1, OUTPUT);
   pinMode(MOTORLEFT_2, OUTPUT);
   pinMode(MOTORRIGHT_1, OUTPUT);
   pinMode(MOTORRIGHT_2, OUTPUT);
-  pinMode(LEFTSENS, INPUT);
-  pinMode(RIGHTSENS, INPUT);
-  pinMode(BACKSENS, INPUT);
-  analogWrite(11, 200);
-  digitalWrite(10, LOW);
   Serial.print("Setup Done");
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
     while (1);
   }
-  while (1);
 }
 
 void loop() {
+  timeThis = millis();
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
   distance = measure.RangeMilliMeter;
@@ -119,74 +51,15 @@ void loop() {
   Serial.print("\t");
   Serial.print(analogRead(1));
   Serial.print("\t");
-  Serial.println(analogRead(2));
+  Serial.print(analogRead(2));
+  Serial.print("\t");
+  Serial.println(state);
+  
   switch (state) {
-    case LINEFOUNDLEFT:
-      reverse();
-      right();
-      if (analogRead(0) <= 500) {
-        state = LINEFOUNDLEFT;
-      }
-      if (analogRead(1) <= 500) {
-        state = LINEFOUNDRIGHT;
-      }
-      if (analogRead(2) <= 500) {
-        state = LINEFOUNDBACK;
-      }
-      else {
-        state = SEARCH;
-      }
-      break;
-    case LINEFOUNDRIGHT:
-      reverse();
-      left();
-      /*if (analogRead(0) <= 500) {
-        state = LINEFOUNDLEFT;
-        }
-        if (analogRead(1) <= 500) {
-        state = LINEFOUNDRIGHT;
-        }
-        if (analogRead(2) <= 500) {
-        state = LINEFOUNDBACK;
-        }*/
-      //else {
-      state = SEARCH;
-      //}
-      break;
-    case LINEFOUNDBACK:
-      forward();
-      if (analogRead(0) <= 500) {
-        state = LINEFOUNDLEFT;
-      }
-      if (analogRead(1) <= 500) {
-        state = LINEFOUNDRIGHT;
-      }
-      if (analogRead(2) <= 500) {
-        state = LINEFOUNDBACK;
-      }
-      else {
-        state = SEARCH;
-      }
-      break;
-    case ROBOTFOUND:
-      forward();
-      if (analogRead(0) <= 500) {
-        state = LINEFOUNDLEFT;
-      }
-      if (analogRead(1) <= 500) {
-        state = LINEFOUNDRIGHT;
-      }
-      if (analogRead(2) <= 500) {
-        state = LINEFOUNDBACK;
-      }
-      if (distance > 1000) {
-        state = SEARCH;
-      }
-      if (distance <= 100) {
-        //liftservo.write(170);
-      }
-      break;
-    case SEARCH:
+    case FORWARD:
+
+
+      timeLast = timeThis;
       if (analogRead(0) <= 500) {
         state = LINEFOUNDLEFT;
       }
@@ -199,21 +72,232 @@ void loop() {
       if (distance <= 1000) {
         state = ROBOTFOUND;
       }
+      if (timeThis - timeLast <= 100) {
+
+        analogWrite(MOTORLEFT_1, 200);
+        digitalWrite(MOTORLEFT_2, LOW);
+        analogWrite(MOTORRIGHT_1, 200);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
       else {
-        right();
-        forward();
-        left();
-        forward();
+        digitalWrite(MOTORLEFT_1, LOW);
+        digitalWrite(MOTORLEFT_2, LOW);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
+      if (direct = SEARCH) {
+        state = LEFT;
+      }
+      else {
+        state = SEARCH;
       }
 
+
+      break;
+
+
+    case REVERSE:
+
+
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      if (distance <= 1000) {
+        state = ROBOTFOUND;
+      }
+      if (timeThis - timeLast <= 100) {
+
+        digitalWrite(MOTORLEFT_1, LOW);
+        analogWrite(MOTORLEFT_2, 200);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        analogWrite(MOTORRIGHT_2, 200);
+      }
+      else {
+        digitalWrite(MOTORLEFT_1, LOW);
+        digitalWrite(MOTORLEFT_2, LOW);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
+      if (direct = LEFT)  {
+        state = RIGHT;
+      }
+      if (direct = RIGHT) {
+        state = LEFT;
+      }
+      else {
+        state = SEARCH;
+      }
+
+
+      break;
+
+
+    case LEFT:
+
+
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      if (distance <= 1000) {
+        state = ROBOTFOUND;
+      }
+      if (timeThis - timeLast <= 250) {
+
+        analogWrite(MOTORLEFT_1, 200);
+        digitalWrite(MOTORLEFT_2, LOW);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        analogWrite(MOTORRIGHT_2, 200);
+      }
+      else {
+        digitalWrite(MOTORLEFT_1, LOW);
+        digitalWrite(MOTORLEFT_2, LOW);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
+      if (direct = SEARCH) {
+        state = FORWARD;
+      }
+      else {
+        state = SEARCH;
+      }
+
+
+      break;
+
+
+    case RIGHT:
+
+
+
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      if (distance <= 1000) {
+        state = ROBOTFOUND;
+      }
+      if (timeThis - timeLast <= 250) {
+
+        digitalWrite(MOTORLEFT_1, LOW);
+        analogWrite(MOTORLEFT_2, 200);
+        analogWrite(MOTORRIGHT_1, 200);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
+      else {
+        digitalWrite(MOTORLEFT_1, LOW);
+        digitalWrite(MOTORLEFT_2, LOW);
+        digitalWrite(MOTORRIGHT_1, LOW);
+        digitalWrite(MOTORRIGHT_2, LOW);
+      }
+      if (direct = SEARCH) {
+        state = LEFT;
+      }
+      else {
+        state = SEARCH;
+      }
+
+
+      break;
+
+
+    case LINEFOUNDLEFT:
+
+
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      direct = LEFT;
+      state = REVERSE;
+      break;
+    case LINEFOUNDRIGHT:
+
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      else {
+        state = SEARCH;
+      }
+      break;
+    case LINEFOUNDBACK:
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      else {
+        state = SEARCH;
+      }
+
+
+      break;
+
+
+    case ROBOTFOUND:
+
+
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      if (distance > 1000) {
+        state = SEARCH;
+      }
+
+
+      break;
+
+
+    case SEARCH:
+
+
+      direct = SEARCH;
+      if (analogRead(0) <= 500) {
+        state = LINEFOUNDLEFT;
+      }
+      if (analogRead(1) <= 500) {
+        state = LINEFOUNDRIGHT;
+      }
+      if (analogRead(2) <= 500) {
+        state = LINEFOUNDBACK;
+      }
+      if (distance <= 1000) {
+        state = ROBOTFOUND;
+      }
+      break;
   }
-
-
-  /*  if (distance <= 50) {
-      liftservo.write(170);
-    }
-    else {
-      liftservo.write(0);
-    }
-  */
 }
